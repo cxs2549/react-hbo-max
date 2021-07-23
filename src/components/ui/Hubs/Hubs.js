72125fedf49f1c47266ecd23f5b34ce7ce4f10ea
cxs2549 <sanchez.cliff.86@gmail.com>
2021-07-23 15:48:15 -0700

    add hubs support for mobile

--- src/components/ui/Hubs/Hubs.js
+++ src/components/ui/Hubs/Hubs.js
@@ -3,34 +3,43 @@ import styled from "styled-components";
 const StyledHubs = styled.div`
   background-color: black;
   width: 100%;
- 
   margin-bottom: 56px;
-  align-items: flex-start;
-  justify-content: space-between;
-  gap: 1rem;
+  /* gap: .25rem; */
   padding: 1rem;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  
   padding-bottom: 4rem;
   @media (min-width: 768px) {
     padding: 1rem 2rem;
+    padding-bottom: 0;
   }
-  @media (min-width: 1024px) {
-    width: 70%;
-  }
+
   #imageWrapper {
-    min-width: 160px;
-    max-width: 160px;
+    min-width: 49%;
     height: 76px;
     padding: 4px 32px;
     border-radius: 99999px;
+    margin-bottom: 1rem;
     background-color: #1fd1f9;
     background-image: linear-gradient(315deg, #1fd1f9 0%, #b621fe 74%);
+    @media (min-width: 768px) {
+      min-width: 19%;
+    }
+    @media (min-width: 1024px) {
+      min-width: 19%;
+    }
+    @media (min-width: 1536px) {
+      min-width: 9%;
+    }
 
     position: relative;
     img {
       height: 100%;
       width: 100%;
       object-fit: contain;
-      padding: .25rem 0;
+      padding: .5rem 0;
     }
     #imageBG {
       position: absolute;
@@ -38,8 +47,8 @@ const StyledHubs = styled.div`
       right: 0;
       top: 50%;
       transform: translate(-50%, -50%);
-      height: 90%;
-      width: 96%;
+      height: 92%;
+      width: 97%;
       background-color: black;
     }
   }
@@ -47,7 +56,7 @@ const StyledHubs = styled.div`
 
 const Hubs = ({ hubs }) => {
   return (
-    <StyledHubs className='grid grid-cols-2 md:grid-cols-3'>
+    <StyledHubs className=''>
       {hubs.map((hub, i) => (
         <div key={i} id="imageWrapper">
           <div className=" rounded-full h-full" id="imageBG">
